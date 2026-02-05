f5d63ef8edc392f8e29e45e5675fd93767e8805e
lxwzone <lxwzone@qq.com>
2026-02-06 00:17:21 +0800

    Add User model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,51 @@
+import mongoose, { Document, Schema } from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+export interface IUser extends Document {
+  name: string
+  email: string
+  password: string
+  createdAt: Date
+  comparePassword(candidatePassword: string): Promise<boolean>
+}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    trim: true,
+    minlength: [2, 'Name must be at least 2 characters']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [6, 'Password must be at least 6 characters'],
+    select: false
+  }
+}, {
+  timestamps: true
+})
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+userSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean> {
+  return await bcrypt.compare(candidatePassword, this.password)
+}
+
+export default mongoose.model<IUser>('User', userSchema)
